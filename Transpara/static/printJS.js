291b5472cdd0f6914e569284ecabc68bdd51e76a
Nayan Patel <nayanpatel00112@gmail.com>
2023-09-10 14:46:58 +0530

    transpara logo

--- Transpara/static/printJS.js
+++ Transpara/static/printJS.js
@@ -1,20 +1,20 @@
 function myprint() {
     var t = document.getElementsByTagName('body')[0].innerHTML
-    document.getElementsByTagName('body')[0].innerHTML = document.getElementById('hed').innerHTML + "" + document.getElementById('PragraphText').innerHTML + document.getElementById('DictWordsTable').innerHTML + "<br><br>" + document.getElementById("footr").innerHTML
+    document.getElementsByTagName('body')[0].innerHTML = document.getElementById('hed').innerHTML + "" + document.getElementById('PragraphText').innerHTML + document.getElementById('DictWordsTable').innerHTML + "<br><br>" + document.getElementById("fr").innerHTML
     window.print()
     document.getElementsByTagName('body')[0].innerHTML = t
 }
 
 function meaningsprint() {
     var t = document.getElementsByTagName('body')[0].innerHTML
-    document.getElementsByTagName('body')[0].innerHTML = document.getElementById('hed').innerHTML + "" + document.getElementById('PragraphText').innerHTML + document.getElementById('divmeaning').innerHTML + "<br><br>" + document.getElementById("footr").innerHTML
+    document.getElementsByTagName('body')[0].innerHTML = document.getElementById('hed').innerHTML + "" + document.getElementById('PragraphText').innerHTML + document.getElementById('divmeaning').innerHTML + "<br><br>" + document.getElementById("fr").innerHTML
     window.print()
     document.getElementsByTagName('body')[0].innerHTML = t
 }
 
 function printWhole() {
     var t = document.getElementsByTagName('body')[0].innerHTML
-    document.getElementsByTagName('body')[0].innerHTML = document.getElementById('hed').innerHTML + "" + document.getElementById('PragraphText').innerHTML + document.getElementById('DictWordsTable').innerHTML + document.getElementById('divmeaning').innerHTML + "<br><br>" + document.getElementById("footr").innerHTML
+    document.getElementsByTagName('body')[0].innerHTML = document.getElementById('hed').innerHTML + "" + document.getElementById('PragraphText').innerHTML + document.getElementById('DictWordsTable').innerHTML + document.getElementById('divmeaning').innerHTML + "<br><br>" + document.getElementById("fr").innerHTML
     window.print()
     document.getElementsByTagName('body')[0].innerHTML = t
 }

7f271ab60b0214e3828598e0f8fceacb18b90be7
Nayan Patel <nayanpatel00112@gmail.com>
2023-09-11 21:16:46 +0530

    delet option in history

--- Transpara/HistoryDisp/urls.py
+++ Transpara/HistoryDisp/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('history', views.history, name="history"),
+    path('delete_histry', views.delete_histry, name="delete_histry"),
 ]
--- Transpara/HistoryDisp/views.py
+++ Transpara/HistoryDisp/views.py
@@ -13,5 +13,15 @@ def history(request):
             return Http404
     else:
         return Http404
-
-    
+def delete_histry(request):
+    if request.user.is_authenticated:
+        try:
+            if request.method == "POST":
+                user_id = request.POST["ID_Delete"]
+                UserData.objects.get(id = user_id).delete()
+                ud = UserData.objects.filter(user_id = request.user.id)
+                return render(request, "history/history.html", {"ud": ud, "active_history":"active"})
+        except:
+            return Http404
+    else:
+        return Http404

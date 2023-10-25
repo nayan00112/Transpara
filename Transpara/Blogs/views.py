dfea4add3fb2e7c543313d83b961c1b4611047c7
Nayan Patel <nayanpatel00112@gmail.com>
2023-10-25 11:25:08 +0530

    blogs updated

--- Transpara/Blogs/views.py
+++ Transpara/Blogs/views.py
@@ -14,18 +14,23 @@ def about(request):
 def contact(request):
     fm = FeedBackForm()
     if request.method=="POST":
-        try:
-            topic_title = request.POST['topic_title']
-            email = request.POST['email']
-            feedbackmessage = request.POST['feedbackmessage']
-            # day month year Hour Minute Second
-            dateandtime = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        if request.user.is_authenticated:
+            try:
+                topic_title = request.POST['topic_title']
+                email = request.user.email
+                feedbackmessage = request.POST['feedbackmessage']
+                # day month year Hour Minute Second
+                dateandtime = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
-            fd = FeedbackData(topic_title = topic_title, email = email, feedbackmessage = feedbackmessage, currenttime = dateandtime, status = False)
-            fd.save()
-            messages.info(request, "Responce saved successfully.")
-        except:
-            messages.error(request, "Error: Not saved responce!")
-        
-    return render (request, "Blogs/contact.html", {"active_contact":"active", 'fm' :fm})
+                fd = FeedbackData(topic_title = topic_title, email = email, feedbackmessage = feedbackmessage, currenttime = dateandtime, status = False)
+                fd.save()
+                messages.info(request, "Responce saved successfully.")
+            except:
+                messages.error(request, "Error: Not saved responce!")
+        else:
+            messages.error(request, "<p style='color:red;'>Login required!</p>")
+    if request.user.is_authenticated:
+        return render (request, "Blogs/contact.html", {"active_contact":"active", 'fm' :fm})
+    else:
+        return render (request, "Blogs/contact.html")
 
